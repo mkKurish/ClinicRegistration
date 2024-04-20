b9d723cfd9af3faa1f227f9044689a5a8f4796e5
mkKurish <kurish04@mail.ru>
2024-04-21 00:44:14 +0300

    LinearSearch developed

--- src/main/java/com/coursework/clinicregistration/Algorithms/LinearSearch.java
+++ src/main/java/com/coursework/clinicregistration/Algorithms/LinearSearch.java
@@ -1,4 +1,21 @@
 package com.coursework.clinicregistration.Algorithms;
 
-public class LinearSearch {
+public class LinearSearch implements TextSearch {
+    @Override
+    public int search(String text, String word) {
+        if (text.length() < word.length()) return -1;
+        int i = 0;
+        while (i + word.length() - 1 < text.length()) {
+            boolean correctWord = true;
+            for (int j = 0; j < word.length(); j++) {
+                if (word.charAt(j) != text.charAt(i + j)) {
+                    correctWord = false;
+                    j = word.length();
+                }
+            }
+            if (correctWord) return i;
+            i++;
+        }
+        return -1;
+    }
 }
