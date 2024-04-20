fe7c903f96f9287e1af12c47241944f18142eca5
mkKurish <kurish04@mail.ru>
2024-04-21 00:44:37 +0300

    QuickSort developed

--- src/main/java/com/coursework/clinicregistration/Algorithms/QuickSort.java
+++ src/main/java/com/coursework/clinicregistration/Algorithms/QuickSort.java
@@ -1,4 +1,40 @@
 package com.coursework.clinicregistration.Algorithms;
 
-public class QuickSort{
+import com.coursework.clinicregistration.DataStructures.DoubleLinkedList;
+
+import java.util.Iterator;
+
+public class QuickSort implements Sorting{
+
+    @Override
+    public <T extends Comparable> DoubleLinkedList<T> sort(DoubleLinkedList<T> source, Class<T> dataClass) {
+        if (source.length <= 1) return source;
+        Iterator<T> slow = source.iterator();
+        Iterator<T> fast = source.iterator();
+        boolean stepFlag = true;
+        T mid = null;
+        while (fast.hasNext()){
+            fast.next();
+            if (stepFlag) mid = slow.next();
+            stepFlag = !stepFlag;
+        }
+
+        DoubleLinkedList<T> leftPart = new DoubleLinkedList<>(dataClass);
+        DoubleLinkedList<T> rightPart = new DoubleLinkedList<>(dataClass);
+
+        Iterator<T> curr = source.iterator();
+        while (curr.hasNext()){
+            T currData = curr.next();
+            if (currData.compareTo(mid) > 0) leftPart.add(currData);
+            if (currData.compareTo(mid) < 0) rightPart.add(currData);
+        }
+        leftPart = sort(leftPart, dataClass);
+        rightPart = sort(rightPart, dataClass);
+        leftPart.add(mid);
+        curr = rightPart.iterator();
+        while (curr.hasNext()){
+            leftPart.add(curr.next());
+        }
+        return leftPart;
+    }
 }
--- src/main/java/com/coursework/clinicregistration/Algorithms/Sorting.java
+++ src/main/java/com/coursework/clinicregistration/Algorithms/Sorting.java
@@ -1,4 +1,7 @@
 package com.coursework.clinicregistration.Algorithms;
 
+import com.coursework.clinicregistration.DataStructures.DoubleLinkedList;
+
 public interface Sorting {
+    <T extends Comparable>  DoubleLinkedList<T> sort(DoubleLinkedList<T> list, Class<T> dataClass);
 }
